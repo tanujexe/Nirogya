67e6f1d065a50a6c51e94b2616b4c32f57de98b1
Tanuj <tanujagrawal1432@gmail.com>
2026-05-14 23:01:41 +0530

    vm

---
+++ backend/src/middleware/validateMiddleware.js
@@ -0,0 +1,12 @@
+const { validationResult } = require('express-validator');
+
+const validate = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    res.status(400);
+    throw new Error(errors.array()[0].msg);
+  }
+  next();
+};
+
+module.exports = validate;
